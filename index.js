dd73ed216322a5d03e40a5ca4a496bb075ee385c
Warm Ronaldo Dude <loliconneko@gmail.com>
2019-10-28 15:51:58 +0530

    Add backward compat

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 'use strict'
 
 module.exports = require('./lib/parser')
+
+// Add backward compat
+const errors = require('redis-errors')
+module.exports.ReplyError = errors.ReplyError;
+module.exports.RedisError = errors.RedisError;
+module.exports.ParserError = errors.ParserError;
